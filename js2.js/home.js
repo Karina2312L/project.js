65dbd47438996dea8df162278e3c4437a4d79c26
username <usermail>
2020-11-06 18:44:17 +0200

    init

---
+++ js2.js/home.js
@@ -0,0 +1,32 @@
+// var day = 7;
+// var kmInDay = 8;
+
+// var result = day*kmInDay;
+
+
+// var day = alert(`Количество дней!`);
+// var kmInDay = alert(`количество км!`);
+// var result = day*kmInDay alert (`result`);
+
+// var day = prompt (`Количество дней!`);
+// var kmInDay = prompt(`кольичество км!`);
+// var formula = day*kmInDay;
+// var result = alert formula;
+
+// var day = prompt (`Количество дней!`);
+// var kmInDay = prompt(`кольичество км!`);
+// var result = day*kmInDay;
+// alert ("itog" + result);
+
+
+do{
+    var day = prompt (`Количество дней!`);
+    var kmInDay = prompt(`кольичество км!`);
+    var result = (day*kmInDay)/2; 
+     alert ("itog" + result);
+}
+while ( result !== null)
+    
+
+  
+
